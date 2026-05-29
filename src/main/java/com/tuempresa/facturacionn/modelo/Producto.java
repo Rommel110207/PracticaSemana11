fe294ddbbc6c878700399cce91f78e7465a42068
Rommel110207 <rommelmg@uamv.edu.ni>
2026-05-29 15:48:34 -0600

    feat: Leccion 1 finalizada.

--- src/main/java/com/tuempresa/facturacionn/modelo/Producto.java
+++ src/main/java/com/tuempresa/facturacionn/modelo/Producto.java
@@ -1,4 +1,13 @@
 package com.tuempresa.facturacionn.modelo;
+import javax.persistence.*;
+import org.openxava.annotations.*;
+import lombok.*;
 
+@Entity @Getter @Setter
 public class Producto {
+    @Id @Column(length=9)
+    int numero;
+
+    @Column(length=50) @Required
+    String descripcion;
 }
